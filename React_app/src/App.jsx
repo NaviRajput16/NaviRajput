916561de097a3ac9cc5705328f99701e7953e069
Navi Rajput <132370616+Nav69kaju@users.noreply.github.com>
2023-08-26 13:18:27 +0530

    Add files via upload

---
+++ React_app/src/App.jsx
@@ -0,0 +1,23 @@
+// function App() {
+
+//   return (
+//     <>
+//       <h1>Hello World</h1>
+//     </>
+//   )
+// }
+
+// export default App
+
+const App =()=>{
+  const styleObj={
+    backgroundColor:'silver',
+    color: 'black',
+    fontSize: '48px',
+    fontWeight: 'bold',
+    padding: 16 ,
+    }
+
+  return <marquee style={styleObj}>Hello World</marquee>
+}
+export default App
