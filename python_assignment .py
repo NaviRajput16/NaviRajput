197930cd8a6f344492a1adc52d06b2738e55af0e
Navi Rajput <132370616+Nav69kaju@users.noreply.github.com>
2023-09-02 07:31:08 +0530

    Add files via upload

---
+++ python_assignment .py
@@ -0,0 +1,17 @@
+# Taking two inputs from the user
+a = float(input("Enter the first number (a): "))
+b = float(input("Enter the second number (b): "))
+
+# Floor Division
+floor_division_result = a // b
+
+# Exponentiation (a power b)
+exponentiation_result = a ** b
+
+# Check if the variables are equal
+are_equal = a == b
+
+# Printing the results
+print(f"Floor Division (a // b): {floor_division_result}")
+print(f"Exponentiation (a ** b): {exponentiation_result}")
+print(f"Are a and b equal? {are_equal}")
